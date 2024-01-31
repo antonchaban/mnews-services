a0ead73c5a3a5ba89d4f227970941f6dde5d1904
Anton Chaban <a.chaban-2027@kpi.ua>
2024-01-31 17:35:39 +0200

    implemented simple rest api with model usage to detect category

--- services/category-detector-ai/categories-api/app.py
+++ services/category-detector-ai/categories-api/app.py
@@ -1,12 +1,64 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import pickle
 
 app = Flask(__name__)
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+label_to_category = {
+    0: "U.S. NEWS",
+    1: "COMEDY",
+    2: "PARENTING",
+    3: "WORLD NEWS",
+    4: "CULTURE & ARTS",
+    5: "TECH",
+    6: "SPORTS",
+    7: "ENTERTAINMENT",
+    8: "POLITICS",
+    9: "WEIRD NEWS",
+    10: "ENVIRONMENT",
+    11: "EDUCATION",
+    12: "CRIME",
+    13: "SCIENCE",
+    14: "WELLNESS",
+    15: "BUSINESS",
+    16: "STYLE & BEAUTY",
+    17: "FOOD & DRINK",
+    18: "MEDIA",
+    19: "QUEER VOICES",
+    20: "HOME & LIVING",
+    21: "WOMEN",
+    22: "BLACK VOICES",
+    23: "TRAVEL",
+    24: "MONEY",
+    25: "RELIGION",
+    26: "LATINO VOICES",
+    27: "IMPACT",
+    28: "WEDDINGS",
+    29: "COLLEGE",
+    30: "PARENTS",
+    31: "ARTS & CULTURE",
+    32: "STYLE",
+    33: "GREEN",
+    34: "TASTE",
+    35: "HEALTHY LIVING",
+    36: "THE WORLDPOST",
+    37: "GOOD NEWS",
+    38: "WORLDPOST",
+    39: "FIFTY",
+    40: "ARTS",
+    41: "DIVORCE"
+}
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/api/category-ai/predict', methods=['POST'])
+def predict_category():
+    data = request.get_json()
+    text = data['text']
+    category_label = model.predict([text])[0] - 1
+    category_name = label_to_category.get(category_label, "Unknown")
+    return jsonify({'category': category_name})
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
