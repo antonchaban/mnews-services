93937ab15b5313828fbabc8259a2e5b1b3af6914
Anton Chaban <a.chaban-2027@kpi.ua>
2024-02-01 11:48:33 +0200

    parsing service init

---
+++ services/parsing-service/src/main/java/a/chaban/fict/parsing/parsingservice/ParsingServiceApplication.java
@@ -0,0 +1,13 @@
+package a.chaban.fict.parsing.parsingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParsingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParsingServiceApplication.class, args);
+    }
+
+}
