d50b0811a8d6582afbdaf5fe23e38e91d414ff30
Anton Chaban <a.chaban-2027@kpi.ua>
2024-06-01 21:55:42 +0300

    Remove secure token service

--- services/secure-token-service/src/main/java/com/a/chaban/sts/repo/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.a.chaban.sts.repo;
-
-import com.a.chaban.sts.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-
-    User findByUsername(String username);
-}
